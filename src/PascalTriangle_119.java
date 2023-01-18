9e03fd564f8664ea61cce0087e1f1d74675ddb06
hondasmx <gainanov.artem@gmail.com>
2023-01-18 14:49:56 +0700

    pascal triangle 118 / 119

---
+++ src/PascalTriangle_119.java
@@ -0,0 +1,51 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Given an integer numRows, return the first numRows of Pascal's triangle.
+ * <p>
+ * In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
+ */
+public class PascalTriangle_119 {
+    public static void main(String[] args) {
+//        System.out.println(getRow(1));
+        System.out.println(getRow(2));
+        System.out.println(getRow(3));
+        System.out.println(getRow(4));
+        System.out.println(getRow(5));
+    }
+
+    public static List<List<Integer>> generate(int numRows) {
+        var result = new ArrayList<List<Integer>>();
+        result.add(List.of(1));
+        if (numRows == 1) {
+            return result;
+        }
+
+        for (int i = 2; i <= numRows; i++) {
+            var row = new ArrayList<Integer>();
+            row.add(1);
+
+
+            if (i > 2) {
+                var prevRow = result.get(i - 2);
+                var index = 1;
+                while (row.size() < i - 1) {
+                    row.add(prevRow.get(index-1) + prevRow.get(index));
+                    index++;
+                }
+            }
+
+            row.add(1);
+            if (row.size() == i) {
+                result.add(row);
+            }
+        }
+
+        return result;
+    }
+    public static List<Integer> getRow(int rowIndex) {
+        var gen = generate(rowIndex + 1);
+        return gen.get(rowIndex);
+    }
+}
